b56515b9ca07ecf1f1bb7c0752ddde6b397b2e5a
zhaojun.wzj <zhaojun.wzj@alibaba-inc.com>
2018-11-08 17:12:57 +0800

    添加单例代码

--- src/main/java/com/sssr/voltile/VolatileTest.java
+++ src/main/java/com/sssr/voltile/VolatileTest.java
@@ -6,6 +6,7 @@ package com.sssr.voltile;
  * 子性和可见性，volatile只提供可见性。
  *
  * 参考：http://www.cnblogs.com/dolphin0520/p/3920373.html
+ * 参考2：https://www.ibm.com/developerworks/cn/java/j-jtp06197.html
  *
  * @author zhaojun.wzj
  * @version $Id VolatileTest.java, v 0.1 2018-10-26 16:23 zhaojun.wzj Exp $$
